048153af93e5b9cb5edf08a24a540af2233f8f07
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-03-05 03:18:06 +0000

    Fix build

--- build_and_release.sh
+++ build_and_release.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euo pipefail -x
+cd "${BASH_SOURCE[0]%/*}"
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --quiet --no-modify-path -y
-~/.cargo/bin/cargo install sccache
+#~/.cargo/bin/cargo install sccache
 sccache_path=$HOME/.cargo/bin/sccache
 set -e
 ( set -x; "$sccache_path" --help )
 set +e
-sccache_version=$( "$sccache_path" | awk '{print $1}' )
+sccache_version=$( "$sccache_path" --version | head -1 | awk '{print $2}' )
+echo "sccache version: $sccache_version"
+
+unix_timestamp_sec=$( date +%s )
+tag="v$sccache_version-$unix_timestamp_sec"
+
+archive_name=sccache-$tag.gz
+
+mkdir -p build
+rm -f build/*
+cd build
 cp "$sccache_path" .
 gzip sccache
-archive_path=sccache.gz
+mv sccache.gz "$archive_name"
 
-sha256sum "$archive_path" >"$archive_path.sha256"
-unix_timestamp_sec=$( date +%s )
+sha256sum "$archive_name" >"$archive_name.sha256"
 
 echo "SHA256 sum:"
-cat "$archive_path.sha256"
+cat "$archive_name.sha256"
 echo
 
-tag="v$sccache_version-$unix_timestamp_sec"
 echo "Using tag: $tag"
 
-set -x
+git_sha1=$( git rev-parse HEAD )
 hub release create "$tag" \
   -m "Release $tag" \
-  -a "$archive_path" \
-  -a "$archive_path.sha256" \
-  -t "$tag"
+  -a "$archive_name" \
+  -a "$archive_name.sha256" \
+  -t "$git_sha1"
