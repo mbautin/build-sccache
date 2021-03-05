8bd61f388ffbdd0a2cad6d39dd37663c223d337d
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-03-04 21:09:53 -0600

    Update build_and_release.sh

--- build_and_release.sh
+++ build_and_release.sh
@@ -11,10 +11,18 @@ sccache_version=$( "$sccache_path" | awk '{print $1}' )
 cp "$sccache_path" .
 gzip sccache
 archive_path=sccache.gz
+
 sha256sum "$archive_path" >"$archive_path.sha256"
 unix_timestamp_sec=$( date +%s )
-tag="v$sccache_version-$unix_timestamp_sec"
 
+echo "SHA256 sum:"
+cat "$archive_path.sha256"
+echo
+
+tag="v$sccache_version-$unix_timestamp_sec"
+echo "Using tag: $tag"
+
+set -x
 hub release create "$tag" \
   -m "Release $tag" \
   -a "$archive_path" \
